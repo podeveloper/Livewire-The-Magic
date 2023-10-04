e788f29eaac8b53e1d1bea73f29d9521f0329c5a
Yasin Korkmaz <theacherx@gmail.com>
2023-10-04 18:43:21 +0300

    Wire Keydown

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -1,11 +1,10 @@
 <form wire:submit.prevent="updateprofile">
     <label for="">Name</label><br><br>
-    <input type="text" wire:model.defer="user.name">
-    <button wire:click="checkname" type="button">Check Name</button>
+    <input type="text" wire:model="user.name" wire:keydown="checkname">
     <br><br>
     @if($errors->has('user.name')) <p style="color:red">{{ $errors->first('user.name') }}</p> @endif
     <label for="">Email</label><br><br>
-    <input type="text" wire:model.defer="user.email"><br><br>
+    <input type="text" wire:model="user.email"><br><br>
     @if($errors->has('user.email')) <p style="color:red">{{ $errors->first('user.email') }}</p> @endif
     <input type="submit" value="Send">
     @if($success)
