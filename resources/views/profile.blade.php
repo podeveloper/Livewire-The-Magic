52d5f1924dd3eaa48b742da10185fe8dbc78e4ff
Yasin Korkmaz <theacherx@gmail.com>
2023-10-04 13:24:49 +0300

    First Livewire Component

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,13 +9,7 @@
     @livewireStyles
 </head>
 <body>
-<form action="">
-    <label for="">Name</label><br><br>
-    <input type="text"><br><br>
-    <label for="">Email</label><br><br>
-    <input type="text"><br><br>
-    <input type="submit" value="Send">
-</form>
+    @livewire('profile')
     @livewireScripts
 </body>
 </html>
