4fdb03319191f54542139c2ef8f8c486cafd8c53
Yasin Korkmaz <theacherx@gmail.com>
2023-10-04 14:32:40 +0300

    Wire Model Lazy Example

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -1,9 +1,9 @@
 <form wire:submit.prevent="updateprofile">
     <label for="">Name</label><br><br>
-    <input type="text" wire:model.debounce.1s="name"><br><br>
+    <input type="text" wire:model.lazy="name"><br><br>
     @if($errors->has('name')) <p style="color:red">{{ $errors->first('name') }}</p> @endif
     <label for="">Email</label><br><br>
-    <input type="text" wire:model.debounce.2s="email"><br><br>
+    <input type="text" wire:model.lazy="email"><br><br>
     @if($errors->has('email')) <p style="color:red">{{ $errors->first('email') }}</p> @endif
     <input type="submit" value="Send">
     @if($success)
