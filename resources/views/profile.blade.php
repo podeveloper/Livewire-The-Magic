3a4fa6e080fdb9991f23166f8274ced9f8ee9e24
Yasin Korkmaz <theacherx@gmail.com>
2023-10-04 13:20:42 +0300

    Basic Form

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,1 +1,7 @@
-test
+<form action="">
+    <label for="">Name</label><br><br>
+    <input type="text"><br><br>
+    <label for="">Email</label><br><br>
+    <input type="text"><br><br>
+    <input type="submit" value="Send">
+</form>
