7f51c93bf8a4ffcb84e85e2e2bf0b335ce9638ba
Yasin Korkmaz <theacherx@gmail.com>
2023-10-04 13:33:56 +0300

    Basic Update

--- app/Http/Livewire/Profile.php
+++ app/Http/Livewire/Profile.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Profile extends Component
 {
+
+    public $name;
+    public $email;
+    public $success = false;
+
+    public function mount()
+    {
+        $this->name = User::first()->name;
+        $this->email = User::first()->email;
+    }
     public function render()
     {
         return view('livewire.profile');
+    }
+
+    public function updateprofile()
+    {
+        User::first()->update([
+            'name' => $this->name,
+            'email' => $this->email
+        ]);
+
+        $this->success = true;
     }
 }
--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -1,7 +1,12 @@
-<form action="">
+<form wire:submit.prevent="updateprofile">
     <label for="">Name</label><br><br>
-    <input type="text"><br><br>
+    <input type="text" wire:model="name"><br><br>
     <label for="">Email</label><br><br>
-    <input type="text"><br><br>
+    <input type="text" wire:model="email"><br><br>
     <input type="submit" value="Send">
+    @if($success)
+        <p>
+            Successfully Updated!
+        </p>
+    @endif
 </form>
